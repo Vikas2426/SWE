d294e4f4710e6a4caacffcb6a381a209724eb393
Vikas Choubey <vikas.cby@gmail.com>
2019-08-24 19:10:40 +0530

    String Permutation

--- stringPermutation.java
+++ stringPermutation.java
@@ -1,10 +1,22 @@
-package com;
-
 public class stringPermutation {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    static void permute(String prefix, String str) {
 
-	}
+        if (str.length() == 0) {
+            System.out.println(prefix);
 
+        } else {
+            for (int i = 0; i < str.length(); i++) {
+                prefix = prefix + str.charAt(i);
+                permute(prefix, str.substring(0, i) + str.substring(i + 1));
+                prefix = prefix.substring(0, prefix.length() - 1);
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        String prefix = "";
+        permute(prefix, "vikas");
+    }
 }
